14e3aed9c4712e5f160e7fd152a1fb0e267700ae
Matthieu Riou <mriou@apache.org>
2009-06-07 13:11:40 -0700

    Insertion in the routing table with eventual bucket split.

--- src/routing.hs
+++ src/routing.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE NoMonomorphismRestriction #-} 
+
 module KTable
-  ( KBucket(..), KTree(..), kinsert,
-    kbleaf, binaries, kdepth, nxor, bucketLength, bucketAll
+  ( KBucket(..), KTree(..), kinsert, kclosest, ktreeSize,
+    kbleaf, binaries, kdepth, nxor, bucketLength, bucketAll, bucketToList
   ) where
 
 import qualified Data.Sequence as S
 import qualified Data.Foldable as F
+import Data.List(nub)
 import Data.Word
 import Data.Bits
 
 import Debug.Trace
 
+-- Kademlia constants k and b, size of buckets and number of bits in the id space 
+-- respectively.
+--
 kdepth = 20
 binaries = 160
 
+-- Data types and helper functions
+--
 newtype KBucket = KBucket (S.Seq Integer)
   deriving (Show, Eq)
 
@@ -26,25 +34,76 @@ bucketInsert (KBucket seq) val = KBucket (seq S.|> val)
 bucketElem (KBucket seq) val = F.elem val seq
 bucketLength (KBucket seq) = S.length seq
 bucketAll fn (KBucket seq) = F.all fn seq
+bucketToList (KBucket seq) = F.toList seq
 
-kinsert pivot kt val = kinsert' pivot kt val (binaries-1)
-
-kinsert' pivot kl@(KLeaf kb) val pos = 
-  if bucketLength kb < kdepth
-    then KLeaf $ bucketInsert kb val
-    else if pos == 0 || (val `nxor` pivot > 2 ^ (pos+1)) || bucketElem kb val
-           then kl 
-           else kinsert' pivot (splitBucket kb pos) val pos
-
-kinsert' pivot (KNode left right) val pos = 
-  if testBit val pos 
-    then KNode left (kinsert' pivot right val (pos-1))
-    else KNode (kinsert' pivot left val (pos-1)) right
-
-splitBucket (KBucket seq) pos = pairToNode $ F.foldl separateVals (S.empty,S.empty) seq
-  where separateVals (lseq, rseq) v =
-          if testBit v pos then (lseq, rseq S.|> v) else (lseq S.|> v, rseq)
-        pairToNode (lseq, rseq) = KNode (kbleaf lseq) (kbleaf rseq)
-
+-- Sets common bits to 0 and differing bits to 1
+--   ex: 101110 `xor` 100101 = 001011
 nxor a b = (a .|. b) `xor` (a .&. b)
+
+-- Inserts the provided node id in the routing table
+--
+kinsert pivot kt nid = update_closest_bucket kt nid insertOrSplit
+  where 
+    insertOrSplit pos kb =
+      if bucketElem kb nid
+        then KLeaf kb
+        else if bucketLength kb < kdepth
+               then KLeaf $ bucketInsert kb nid
+               else if pos == 0 || (nid `nxor` pivot > 2 ^ (pos+1))
+                      then KLeaf kb
+                      else traverseKTree KNode KNode pos (splitBucket kb pos) nid insertOrSplit
+
+    splitBucket (KBucket seq) pos = pairToNode $ F.foldl separateVals (S.empty,S.empty) seq
+      where separateVals (lseq, rseq) v =
+              if testBit v pos then (lseq, rseq S.|> v) else (lseq S.|> v, rseq)
+            pairToNode (lseq, rseq) = KNode (kbleaf lseq) (kbleaf rseq)
+
+-- Finds at least k nodes closest to the provided id in the routing table. 
+--
+kclosest kt nid = with_closest_bucket kt nid (returnOrRewind [] nid)
+  where
+    -- If the closest k bucket has at least k element, we're good we can return
+    -- them. Otherwise we have to rewind up the tree to find more nodes.
+    returnOrRewind karr mid pos kb =
+      if (length newkarr) >= kdepth
+        then newkarr
+        else rewind newkarr mid pos
+      where newkarr = nub $ bucketToList kb ++ karr
+
+    -- Flips the current bit and redo the closest bucket search to explore other
+    -- branches. If the bit has already been flipped (comparing to the node we were
+    -- originally looking for), we try to flip higher bits instead.
+    rewind karr mid pos =
+      -- trace ("rewd " ++ show pos) $
+      if pos >= binaries
+        then karr
+        else if (nid .&. 2^pos) `nxor` (mid .&. 2^pos) > 0
+               then {-trace ("rew " ++ show pos) $ -} rewind karr mid (pos+1)
+               else {-trace ("close " ++ show pos) $ -} with_closest_bucket kt newmid (returnOrRewind karr newmid)
+      where newmid = mid `xor` 2^pos
+
+-- Finds the closest bucket to an id and apply the provided transformation 
+-- function (from KBucket to KTree) on it.
+update_closest_bucket :: (Bits a) => KTree -> a -> (Int -> KBucket -> KTree) -> KTree
+update_closest_bucket = traverseKTree KNode KNode (binaries-1)
+
+-- Finds the closest bucket to an id and apply the provided function on it.
+--
+with_closest_bucket :: (Bits a) => KTree -> a -> (Int -> KBucket -> t) -> t
+with_closest_bucket = traverseKTree (flip const) const (binaries-1)
+
+-- Total number of nodes stored in all buckets
+--
+ktreeSize (KNode left right) = ktreeSize left + ktreeSize right
+ktreeSize (KLeaf kb)         = bucketLength kb
+
+-- Goes down the tree by following the provided node id bit track and calls
+-- a provided function once a matching bucket has been found.
+--
+traverseKTree :: (Bits a) => (KTree -> t -> t) -> (t -> KTree -> t) -> Int -> KTree -> a -> (Int -> KBucket -> t) -> t
+traverseKTree consRight consLeft pos (KNode left right) nid fn =
+  if testBit nid pos 
+    then consRight left (traverseKTree consRight consLeft (pos-1) right nid fn)
+    else consLeft (traverseKTree consRight consLeft (pos-1) left nid fn) right
+traverseKTree _ _ pos kl@(KLeaf kb) nid fn = fn pos kb
 
--- test/Test.hs
+++ test/Test.hs
@@ -1,15 +1,18 @@
 import KTable
 
 import Test.QuickCheck.Parallel
---import Test.QuickCheck.Batch
+-- import Test.QuickCheck.Batch
 import Test.QuickCheck
 
 import qualified Data.Sequence as S
 import qualified Data.Foldable as F
 import Data.List(nub)
+import Data.Bits
+import System.Random(randomRIO)
+import Debug.Trace
 
 -- options = TestOptions
---   { no_of_tests         = 50
+--   { no_of_tests         = 200
 --   , length_of_tests     = 0
 --   , debug_tests         = False }
 -- 
@@ -17,26 +20,29 @@ import Data.List(nub)
 --   runTests "ktree" options
 --     [ run prop_max_k_bucket
 --     , run prop_all_proper_bits
---     , run prop_no_duplicates ]
+--     , run prop_no_duplicates
+--     , run prop_kclosest_k ]
 
 
 main = do
-  pRun 2 50 [ ("buckets max", pDet prop_max_k_bucket)
+  t <- randomRIO (0,100000)
+  pRun t 100 [ ("buckets max", pDet prop_max_k_bucket)
             , ("buckets bits position", pDet prop_all_proper_bits)
-            , ("no duplicate", pDet prop_no_duplicates) ]
+            , ("no duplicate", pDet prop_no_duplicates)
+            , ("k retrieval", pDet prop_kclosest_k)
+            , ("kclosest close", pDet prop_kclosest_close) ]
 
 --
 -- Quickcheck definitions and property checkers for KTree
 --
 
 instance Arbitrary KTree where
-  arbitrary = do
-    ks <- choose (1,2000) :: Gen Int
-    case ks of
-      1 -> return $ kbleaf S.empty
-      otherwise -> do t <- arbitrary
-                      v <- choose (0, 2^binaries-1) :: Gen Integer
-                      return $ kinsert 0 t v
+  arbitrary = sized (\x -> treeBuild (x * 20))
+
+treeBuild 0 = return $ kbleaf S.empty
+treeBuild n = do t <- treeBuild (n-1)
+                 v <- choose (0, 2^binaries-1) :: Gen Integer
+                 return $ kinsert 0 t v
 
 -- Checks that all buckets have k or less elements
 prop_max_k_bucket (KNode l r) = prop_max_k_bucket l && prop_max_k_bucket r
@@ -55,3 +61,15 @@ prop_all_proper_bits kn@(KNode _ _) = prop_all_proper_bits' kn 0 binaries
 prop_no_duplicates (KNode l r) = prop_no_duplicates l && prop_no_duplicates r
 prop_no_duplicates (KLeaf (KBucket s)) = nub sl == sl
   where sl = F.toList s
+
+-- Checks that kclosest finds a least k nodes
+prop_kclosest_k:: KTree -> Integer -> Bool
+prop_kclosest_k kt nid = let kc = kclosest kt nid
+                         in length kc >= kdepth || length kc == ktreeSize kt
+
+-- Checks kclosest are actually close to pivot
+prop_kclosest_close kt nid = all (\x -> x `xor` nid > kmaxDist || x `elem` kc) $ allNodes kt
+  where kmaxDist = maximum $ map (xor nid) kc
+        allNodes (KNode l r) = allNodes l ++ allNodes r
+        allNodes (KLeaf kb) = bucketToList kb
+        kc = kclosest kt nid
